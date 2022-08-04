07a31c47cc8158def10f3974947077a62e346eae
Courtney Webster <courtney@atlmd.net>
2022-08-03 21:06:54 -0400

    adding tests for event data

--- src/Event.js
+++ src/Event.js
@@ -1,13 +1,27 @@
 import React, { Component } from "react";
 
 class Event extends Component {
+  constructor() {
+    super();
+    this.state = {
+      show: false
+    };
+  }
+
   toggleDetails = () => {
     let string = this.state.show ? "View details" : "Hide details";
     this.setState({ buttonText: string, show: !this.state.show });
   };
 
   render() {
-    return <div></div>;
+    const { event } = this.props;
+
+    return (
+      <div className="event">
+        <h1 className="event-title">{event.summary}</h1>
+        <div className="event-info"></div>
+      </div>
+    );
   }
 }
 export default Event;
--- src/__tests__/Event.test.js
+++ src/__tests__/Event.test.js
@@ -1,14 +1,20 @@
-// import React from "react";
-// import { shallow } from "enzyme";
-// import { mockData } from "../mock-data";
-// import Event from "../Event";
+import React from "react";
+import { shallow } from "enzyme";
+import { mockData } from "../mock-data";
+import Event from "../Event";
 
-// describe("<Event /> component", () => {
-//   let event, EventWrapper;
-//   beforeAll(() => {
-//     event = mockData[0];
-//     EventWrapper = shallow(<Event event={event} />);
-//   });
+describe("<Event /> component", () => {
+  let event, EventWrapper;
+  beforeAll(() => {
+    event = mockData[0];
+    EventWrapper = shallow(<Event event={event} />);
+  });
 
-//   test('render the title in event item')
-// });
+  test("render title in the event item", () => {
+    expect(EventWrapper.find(".event-title")).toHaveLength(1);
+  });
+
+  test("render information in the event item", () => {
+    expect(EventWrapper.find(".event-info")).toHaveLength(1);
+  });
+});
